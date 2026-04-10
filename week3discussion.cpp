6d6241c69b5a779ed9a15c841537f316dacddcc2
Drew Marshall <drewwinkles@gmail.com>
2026-04-09 18:34:24 -0700

    week 3 discussion on repetition

---
+++ week3discussion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i
+
+    // The Loop
+    for(i = 1, i <= 1, i++) {
+        cout << "Welcome to the program!" << endl;
+        cout << "What's your name?" << endl;
+        cin >> name;
+        if(!name){
+            cout << "Error: No name entered." << endl;
+        } else {
+            cout << "Hello, " << name << "!" << endl;
+        }
+    }
+}
