7e2753389fc92762b43441a2acbc522a9be4aed4
Drew Marshall <drewwinkles@gmail.com>
2026-04-16 18:14:39 -0700

    Module 5 Discussion code

---
+++ Week 5/module5_discussion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std;
+
+// User-defined function
+int addNumbers(int a, int b) {
+    int result = a + b
+    return result;
+}
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    // Calling the function
+    int sum = addNumbers(x, y);
+
+    cout << "Sum is: " << sum << endl
+
+    return 0;
+}
